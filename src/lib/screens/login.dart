84f6d2ffe9fadc4156aa37cc0ce8e828a73d1e12
DevanHoover <142552755+DevanHoover@users.noreply.github.com>
2025-01-27 16:04:38 -0600

    style: Created a basic login form
    Stylized the login form to look more professional

--- src/lib/screens/login.dart
+++ src/lib/screens/login.dart
@@ -11,24 +11,53 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Register'),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: const Offset(0,5),
+              ),
+            ],
+          ),
+         child: Form(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 20),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Username'),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Password')
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Register'),
             ),
             InkWell(
               onTap: () {},
-              child: const Text('Login instead'),
-            )
+              child: const Text('Login instead',
+              style: TextStyle(color: Colors.blue)
+              ),
+            ),
           ],
         ),
       ),
+        )
+      )
     );
   }
 }
