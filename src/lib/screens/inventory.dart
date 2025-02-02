39bc15942a0bca69b546caf13616c6345cb82a95
Brady Reynolds <bradyreynolds05@gmail.com>
2025-02-02 14:51:05 -0600

    feat: simple, unused inventory file

---
+++ src/lib/screens/inventory.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InventoryPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to the Inventory'),
+        backgroundColor: Colors.brown,
+      ),
+    );
+  }
+}
