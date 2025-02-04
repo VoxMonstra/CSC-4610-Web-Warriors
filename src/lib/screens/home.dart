74b258e9081d70c54a51822c8a427ec697cd39f6
noahn <ncgregory43@tntech.edu>
2025-02-03 21:27:26 -0600

    Feat: make homepage scrollable (we will need more content for it to scroll)

--- src/lib/screens/home.dart
+++ src/lib/screens/home.dart
@@ -7,41 +7,46 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       drawer: AppDrawer(currPage: Text('home')),
-      body: Column(
-        children: [
-          // Main content
-          Expanded(
-            child: Center(
-              child: Text(
-                'Welcome to Simple Bakery!',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Main content
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Center(
+                child: Text(
+                  'Welcome to Simple Bakery!',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
 
-          // About section at the bottom
-          Container(
-            padding: EdgeInsets.all(16),
-            color: Colors.white,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'About',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            // About section at the bottom
+            Container(
+              padding: EdgeInsets.all(16),
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'About',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Simple Bakery is your go-to place for delicious baked goods. ',
-                  style: TextStyle(fontSize: 14),
-                ),
-              ],
+                  SizedBox(height: 8),
+                  Text(
+                    'Simple Bakery is your go-to place for delicious baked goods. ',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            //Add more content here in order to keep scrolling
+            SizedBox(height: 500, child: Center(child:Text('More content here')))
+          ],
+        ),
       ),
     );
   }
